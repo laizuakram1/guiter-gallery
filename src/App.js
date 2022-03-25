21679fc8d359ef41aa2ef3338007840e99013c72
laizuakram1 <laizuakram98@gmail.com>
2022-03-25 21:20:32 +0600

    installed react bootstrap

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h2 className='text-primary'>Assignment koratasi- 8</h2>
+    </div>
+  );
+}
+
+export default App;
