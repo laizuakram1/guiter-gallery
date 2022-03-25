0d8465dabebd2d80d1febc402ce5bd811046194f
laizuakram1 <laizuakram98@gmail.com>
2022-03-26 00:31:19 +0600

    add button icon from react fontawesome

--- src/components/Guiter/Guiter.js
+++ src/components/Guiter/Guiter.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Guiter.css';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 const Guiter = ({guiter}) => {
     const {name, price, image} = guiter;
     return (
         <div className='guiter'  >
             <img width="200px" height="250px" src={image} alt="" />
-            <div className='guiter-info'>
+            <div className='guiter-info mt-4'>
              <h4>{name}</h4>
             <p>price:${price}</p>
             </div>
 
-          <Button className='addCartBtn'>Add To Cart</Button>
+          <Button className='addCartBtn'>Add To Cart
+         <span className='addCartIcon'> <FontAwesomeIcon icon ={faShoppingCart}></FontAwesomeIcon></span>
+          </Button>
          
         </div>
         
